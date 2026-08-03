7a1618f2f47ee49449d0d31a87ad3063c4c1aa15
jamalt23 <jamal.tey@icloud.com>
2026-08-03 12:25:08 +0400

    Refactor project structure: update index.html metadata, remove react-router, and implement App component

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Footer from './components/Footer';
+import Header from './components/Header';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+
+      <Footer />
+    </>
+  );
+};
+
+export default App;
