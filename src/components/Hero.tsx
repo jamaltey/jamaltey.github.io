67e2566e234ab1847a33a1a2c6cdccbdd7edf556
jamalt23 <jamal.tey@icloud.com>
2026-08-07 16:24:04 +0400

    Refactor Hero component: update structure and improve semantics by replacing <p> tags with <h1> and <h2>, adjust animation duration, and enhance background styling

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,21 +10,24 @@ const Hero = () => {
 
   return (
     <>
-      <div ref={ref} className="container flex h-screen items-center p-8 md:p-5" id="hero">
+      <div ref={ref} className="isolate container flex h-screen items-center p-8 md:p-5" id="hero">
         <div className="">
-          <p className="pb-1 pl-2 font-mono text-xl md:text-3xl">Hello(); I'm</p>
-          <p className="mb-2 text-4xl font-bold md:text-6xl">Jamal Teymurov</p>
+          <h2 className="pb-1 pl-2 font-mono text-xl md:text-3xl">Hello(); I'm</h2>
+          <h1 className="pb-4 text-4xl font-bold md:text-6xl">Jamal Teymurov</h1>
+          <h2 className="pl-1 text-xl md:text-3xl">A Full-stack web developer</h2>
         </div>
         <a
           href="#about"
           className={cn(
-            'absolute bottom-3 left-1/2 -translate-x-1/2 animate-bounce duration-150',
+            'absolute bottom-3 left-1/2 -translate-x-1/2 animate-bounce duration-200',
             inView ? 'opacity-100' : 'opacity-0'
           )}
         >
           <ArrowDown size={30} />
         </a>
-        <div className="absolute -z-1 h-[80vh] w-70 animate-pulse rounded-full bg-blue-900 blur-[25rem] lg:right-[-15%] lg:w-150"></div>
+        <div className="absolute top-1/2 right-[-20%] -z-10 -translate-y-1/2 animate-pulse [animation-duration:5s] lg:right-[-12%]">
+          <div className="pointer-events-none h-10 w-15 scale-x-[2] scale-y-[9] rounded-full bg-blue-900/60 blur-3xl will-change-[filter] md:w-md"></div>
+        </div>
       </div>
     </>
   );
