aec79c852761aaa7d08318d8be4295e8e327ac37
jamalt23 <jamal.tey@icloud.com>
2026-08-08 15:10:54 +0400

    Refactor Hero component: adjust positioning and structure for improved layout

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,7 +10,7 @@ const Hero = () => {
 
   return (
     <>
-      <div ref={ref} className="container flex h-screen items-center p-8 md:p-5" id="hero">
+      <div ref={ref} className="relative container flex h-screen items-center p-8 md:p-5" id="hero">
         <div className="">
           <h2 className="pb-1 pl-2 font-mono text-xl md:text-3xl">Hello(); I'm</h2>
           <h1 className="pb-4 text-4xl font-bold md:text-6xl">Jamal Teymurov</h1>
@@ -19,15 +19,15 @@ const Hero = () => {
         <a
           href="#about"
           className={cn(
-            'absolute bottom-3 left-1/2 -translate-x-1/2 animate-bounce duration-200',
+            'absolute bottom-2 left-1/2 -translate-x-1/2 animate-bounce duration-200',
             inView ? 'opacity-100' : 'opacity-0'
           )}
         >
           <ArrowDown size={30} />
         </a>
-        <div className="absolute top-1/2 right-6 -z-10 -translate-y-1/2 animate-pulse [animation-duration:5s] lg:right-[-12%]">
-          <div className="pointer-events-none h-10 w-15 scale-x-[4] scale-y-[20] rounded-full bg-blue-900/60 blur-xl will-change-[filter] md:w-md md:scale-x-[3] md:blur-3xl" />
-        </div>
+      </div>
+      <div className="absolute top-[50vh] right-6 -z-10 animate-pulse [animation-duration:6s] sm:right-[-30%] lg:right-[-20%]">
+        <div className="pointer-events-none h-10 w-15 scale-x-[5] scale-y-[8] rounded-full bg-blue-900/60 blur-xl will-change-[filter] sm:w-25 sm:scale-y-[20] sm:blur-3xl md:w-md md:scale-x-[2] lg:scale-x-[3]" />
       </div>
     </>
   );
