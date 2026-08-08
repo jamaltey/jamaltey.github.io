eb793ad026bf7f331c10c8b3f1369751adb3347f
jamalt23 <jamal.tey@icloud.com>
2026-08-08 14:14:57 +0400

    Update Vite configuration to enable host option for the development server

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: { open: true },
+  server: { open: true, host: true },
 });
