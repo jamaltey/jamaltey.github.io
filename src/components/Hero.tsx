b82233af0fc793d28dc9fdc2f63455065cbfc0d2
jamalt23 <jamal.tey@icloud.com>
2026-08-15 01:51:49 +0400

    Enhance Hero component: add z-index to section for improved layering; remove unnecessary z-index from background div

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
       <section
         data-aos="fade-down"
         data-aos-duration={700}
-        className="relative flex h-screen items-center justify-center sm:justify-start"
+        className="relative z-1 flex h-screen items-center justify-center sm:justify-start"
         id="hero"
       >
         <div>
@@ -25,7 +25,7 @@ const Hero = () => {
           <ArrowDown size={30} />
         </a>
       </section>
-      <div className="absolute -top-20 right-0 -z-10 h-[120vh] w-screen scale-150 animate-pulse bg-radial-[at_70%] from-blue-950/70 to-transparent to-50% will-change-transform [animation-duration:5s] md:right-[-15%] lg:w-[70vw]" />
+      <div className="absolute -top-20 right-0 h-[120vh] w-screen scale-150 animate-pulse bg-radial-[at_70%] from-blue-950/70 to-transparent to-50% will-change-transform [animation-duration:5s] md:right-[-15%] lg:w-[70vw]" />
     </>
   );
 };
