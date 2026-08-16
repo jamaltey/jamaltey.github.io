e9d833ae6f394dacfaf35a7a73e8489d579f5852
jamalt23 <jamal.tey@icloud.com>
2026-08-17 01:52:08 +0400

    Enhance Footer and Projects components: Update layout for improved responsiveness; adjust styles for better visual consistency; fix copyright year display in Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,20 +2,22 @@ import { MailIcon } from 'lucide-react';
 
 const Footer = () => {
   return (
-    <footer className="flex h-90 items-center justify-center rounded-t-[50px] border-t border-[#292929] bg-zinc-950 text-center">
-      <div id="contact">
+    <footer className="relative flex min-h-90 flex-col items-center justify-center px-4 rounded-t-[50px] border-t border-[#292929] bg-zinc-950 text-center">
+      <section className="mb-5" id="contact">
         <h3 className="text-4xl text-white md:text-5xl" data-aos="fade-up" data-aos-duration={600}>
           Get in touch
         </h3>
         <a
           href="mailto:hello@jamalteymurov.com"
-          className="mt-3 flex items-center justify-center gap-1.5 text-lg font-medium tracking-wider"
+          className="mt-3 flex text-lg font-medium tracking-wider"
           data-aos="fade-up"
           data-aos-duration={600}
           data-aos-delay={50}
         >
-          <MailIcon />
-          hello@jamalteymurov.com
+          <span className="inline-flex items-center justify-center gap-1.5 duration-250 hover:scale-102">
+            <MailIcon />
+            hello@jamalteymurov.com
+          </span>
         </a>
         <div
           className="mt-5 flex justify-center gap-4"
@@ -28,7 +30,7 @@ const Footer = () => {
             target="_blank"
             rel="noreferrer"
             aria-label="LinkedIn"
-            className="flex size-13 items-center justify-center rounded-full border border-white text-white duration-250 hover:scale-103 hover:bg-zinc-900"
+            className="flex size-13 items-center justify-center rounded-full border border-white text-white duration-250 hover:scale-104 hover:bg-zinc-800/60"
           >
             <img className="ml-1.25" src="logos/linkedin.svg" alt="" />
           </a>
@@ -37,12 +39,15 @@ const Footer = () => {
             target="_blank"
             rel="noreferrer"
             aria-label="GitHub"
-            className="flex size-13 items-center justify-center rounded-full border border-white text-white duration-250 hover:scale-103 hover:bg-zinc-900"
+            className="flex size-13 items-center justify-center rounded-full border border-white text-white duration-250 hover:scale-104 hover:bg-zinc-800/60"
           >
             <img src="logos/github.svg" alt="" />
           </a>
         </div>
-      </div>
+      </section>
+      <p className="absolute bottom-4 text-gray-500/60">
+        &copy; {new Date().getFullYear()} Jamal Teymurov
+      </p>
     </footer>
   );
 };
