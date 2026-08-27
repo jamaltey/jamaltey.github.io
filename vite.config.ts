1712fff6bcf07b6f5fcfa766584426c845c08f4a
jamalt23 <jamal.tey@icloud.com>
2026-08-27 21:21:36 +0400

    Add rollup-plugin-visualizer for improved build visualization

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
+import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), visualizer()],
   server: { open: true, host: true },
 });
